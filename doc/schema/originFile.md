cec0bebbd2bb6c59fc62820ee4079d449b17e769
overlyDramatic <maxjunedown@live.com>
2020-10-10 16:44:27 +0800

    read all sub paths from root path 从选中的根目录下读取相关文件夹

---
+++ doc/schema/originFile.md
@@ -0,0 +1,21 @@
+
+## originFiles
+```js
+[
+  {
+    filename: 'music.mp3',
+    path: 'D:/abc/defg/music.mp3',
+    title: 'music',
+    artist: 'abba',
+    album: 'baab'
+  }
+]
+```
+## originPath
+dirs under the root path that have audio files
+```js
+[
+  'D:/abc',
+  'D:/abc/defg'
+]
+```
--- src/main/action/file.background.js
+++ src/main/action/file.background.js
@@ -1,6 +1,8 @@
 import { ipcMain, dialog } from 'electron'
 import { promises as fsPromises } from 'fs'
-import { normalize } from 'path'
+import { normalize as _normalize, join as _join } from 'path'
+
+const regAudioFile = /.+(\.mp3|\.Mp3|\.flac)$/
 
 export function initOpenDir(currentWindow) {
   ipcMain.on('open-dir', async function(event) {
@@ -19,19 +21,45 @@ function openDirDialog(currentWindow) {
       if (res.canceled) {
         return null
       }
-      const fileData = await readFilesInDir(res.filePaths[0])
+      const fileData = await readDirs(res.filePaths[0])
       return fileData
     })
 }
 
 // TODO 递归读取指定目录下音频文件
-// * recursively read files info from dir
-async function readFilesInDir(dir) {
+// * recursively read dirs
+export async function readDirs(dir) {
+  const _dir = _normalize(dir)
+  const pathSet = new Set()
   try {
-    const files = await fsPromises.readdir(normalize(dir))
-    return files
+    const fileArray = await fsPromises.readdir(_dir)
+    // TODO log empth dir 用日志记录空目录
+    if (!fileArray.length) {
+      console.log('nothing', _dir)
+    }
+    for (const index in fileArray) {
+      const file = fileArray[index]
+      const _file = _join(_dir, file)
+      // * is _file dir or file?
+      const fileStat = await fsPromises.stat(_file)
+      if (fileStat.isDirectory()) {
+        const subPathSet = await readDirs(_file)
+        subPathSet.forEach(item => {
+          pathSet.add(item)
+        })
+      } else if (regAudioFile.test(file)) {
+        if (pathSet.has(_dir)) {
+          break
+        }
+        pathSet.add(_dir)
+      } else {
+        // TODO log not audio file 用日志记录文件不是音频文件
+        console.log('not audio file', _file, '\n')
+      }
+    }
+    return Array.from(pathSet)
   } catch (err) {
     console.error(err)
-    return null
+    return Array.from(pathSet)
   }
 }
